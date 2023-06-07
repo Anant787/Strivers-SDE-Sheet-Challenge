26af098093699e3c554a8ba860ca3fa42f9cc649
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-06-07 12:21:00 +0530

    Create 08.Merge Intervals.cpp
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.

---
+++ 08.Merge Intervals.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+/*
+
+    intervals[i][0] = start point of i'th interval
+    intervals[i][1] = finish point of i'th interval
+
+*/
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    // Write your code here.
+    vector<vector<int>>v;
+    if(intervals.size() == 0)
+    {
+        return v;
+    }
+
+    sort(intervals.begin(),intervals.end());
+    vector<int>temp = intervals[0];
+
+    for(auto it : intervals)
+    {
+        if(it[0] <= temp[1])
+        {
+            temp[1] = max(it[1],temp[1]);
+        }
+        else
+        {
+            v.push_back(temp);
+            temp = it;
+        }
+    }
+    v.push_back(temp);
+    return v;
+}
+
