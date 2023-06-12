a9470b398af7a50bdb7295c839aa26ab59f70eff
Anant gupta <91842337+Anant787@users.noreply.github.com>
2023-06-12 19:45:40 +0530

    Create 62.Matrix Median.cpp
    You have been given a matrix of ‘N’ rows and ‘M’
    sorted in non-decreasing order. Your task is to find
    the overall median of the matrix i.e if all elements of
    the matrix are written in a single line, then you need
    to return the median of that linear array.
    
    The median of a finite list of numbers is the "middle"
    number when those numbers are listed in order from
    smallest to greatest. If there is an odd number of
    observations, the middle one is picked. For example,
    consider the list of numbers [1, 3, 3, 6, 7, 8, 9]. This
    list contains seven numbers. The median is the fourth
    elma alen neon

---
+++ 62.Matrix Median.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+int getMedian(vector<vector<int>> &matrix)
+{
+    // Write your code here.
+    int start=INT_MAX;
+
+    int end=INT_MIN;
+
+    int n=matrix.size();
+
+    int m=matrix[0].size();
+
+    for(int i=0;i<n;i++){
+
+        start=min(start,matrix[i][0]);
+
+        end=max(end,matrix[i][m-1]);
+
+    }
+
+    int midpos=(n*m+1)/2;
+
+    while(start<=end){
+
+        int mid=start+(end-start)/2;
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+
+            int index=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+
+            count+=index;
+
+        }
+
+        if(count<midpos){
+
+            start=mid+1;
+
+        }else{
+
+            end=mid-1;
+
+        }
+
+    }
+
+    return start;
+}
